77a2fb5c2d3d24fe1c623e8e8e462c5abc1b7b06
ngkjon <ngkjon@gmail.com>
2017-05-27 02:03:29 -0400

    tabs

--- SpendTracker/app/src/main/java/enghacks2017/myapplication/MainActivity.java
+++ SpendTracker/app/src/main/java/enghacks2017/myapplication/MainActivity.java
@@ -1,13 +1,42 @@
 package enghacks2017.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_camera:
+                    mTextMessage.setText(R.string.tab_camera);
+                    return true;
+                case R.id.navigation_analytics:
+                    mTextMessage.setText(R.string.tab_analytics);
+                    return true;
+            }
+            return false;
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
 }
